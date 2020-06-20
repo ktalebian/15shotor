1d15840fe3b18acda9f3d9d1f24d1fb965f712fd
Kousha Talebian <kousha.talebian@gmail.com>
2020-06-20 15:12:55 -0700

    add parser and emailer

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,18 @@ type AnimalData = {
 };
 type Animal = keyof AnimalData;
 
-export default function Shotor(username: string, password: string, documentSid: string) {
+let twilio = null;
+const getTwilio = (username: string, password: string): Twilio => {
+  if (twilio) {
+    return twilio;
+  }
+
+  twilio = new Twilio(username, password);
+
+  return twilio;
+};
+
+export const Shotor = (username: string, password: string, documentSid: string) => {
   if (!username || !password) {
     throw new Error('username/password are required');
   }
@@ -18,8 +29,8 @@ export default function Shotor(username: string, password: string, documentSid: 
     throw new Error('Document identifier is required');
   }
 
-  const twilio = new Twilio(username, password);
-  const syncDocument = twilio.sync.services('default').documents(documentSid);
+  const client = getTwilio(username, password);
+  const syncDocument = client.sync.services('default').documents(documentSid);
 
   const getState = async () => syncDocument.fetch();
   const getData = async (): Promise<AnimalData> => getState().then((resp) => resp.data);
@@ -60,4 +71,55 @@ export default function Shotor(username: string, password: string, documentSid: 
     hasTimer,
     setTimer,
   };
-}
+};
+
+export const Parser = () => {
+  const getTimer = (message: string) => {
+    if (!message || message === '') {
+      return -1;
+    }
+
+    const match = message
+      .trim()
+      .toLowerCase()
+      .match(/badbakht mishi in (\d*):(\d*):(\d*)/);
+    if (!match || match.length !== 4) {
+      return -1;
+    }
+
+    return (
+      Date.now() + 1000 * (parseInt(match[1], 10) * 86400 + parseInt(match[2], 10) * 3600 + parseInt(match[3], 10) * 60)
+    );
+  };
+
+  return {
+    getTimer,
+  };
+};
+
+export const Messenger = (username: string, password: string, from: string) => {
+  if (!username || !password) {
+    throw new Error('username/password are required');
+  }
+  if (!from) {
+    throw new Error('From number is required');
+  }
+
+  const client = getTwilio(username, password);
+  const sendMessage = async (body: string, to: string) => client.messages.create({ body, to, from });
+
+  const congratulations = async (animal: Animal, to: string) => {
+    const emoji = animal === 'shotor' ? '🐪' : '🐏';
+    return sendMessage(`Congratulations 🎉! You got a ${emoji}!`, to).then(() => true);
+  };
+
+  const timerSet = async (animal: Animal, timestamp: number, to: string) => {
+    const date = new Date(timestamp).toString();
+    return sendMessage(`Okh okh! You'll be badbakht on ${date}`, to).then(() => true);
+  };
+
+  return {
+    congratulations,
+    timerSet,
+  };
+};
